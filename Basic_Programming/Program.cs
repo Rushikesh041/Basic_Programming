3367ef8c4300fa4e6a914352db0a6b4aba86f6f8
Rushikesh041 <jadhavrushi041@gmail.com>
2023-01-05 21:51:03 +0530

    [Rushi]SwapNumbers

--- Basic_Programming/Program.cs
+++ Basic_Programming/Program.cs
@@ -1,10 +1,20 @@
 ﻿namespace Basic_Programming
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Wel Coome to Basic Programming ");
+            Console.WriteLine("Enter Number 1 : ");
+            int Number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Number 2 : ");
+            int Number2 = Convert.ToInt32(Console.ReadLine());
+
+            int Number3 = Number1;
+                Number1 = Number2;
+                Number2 = Number3;
+
+            Console.WriteLine("Number 1 is : " + Number1);
+            Console.WriteLine("Number 2 is : " + Number2);
         }
     }
 }
