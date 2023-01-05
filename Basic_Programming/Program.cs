b2d5345c2a4a6bfe1aae15d25dee2eab922855b3
Rushikesh041 <jadhavrushi041@gmail.com>
2023-01-05 20:00:58 +0530

    [Rushi]QuotientandRemainder

--- Basic_Programming/Program.cs
+++ Basic_Programming/Program.cs
@@ -1,10 +1,19 @@
 ﻿namespace Basic_Programming
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter Dividend : ");
+            int Dividend = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Divisor : ");
+            int Divisor = Convert.ToInt32(Console.ReadLine());
+
+            int Quotient = Dividend / Divisor;
+            int Remainder = Dividend % Divisor;
+
+            Console.WriteLine("Quotient is : " + Quotient);
+            Console.WriteLine("Remainder is : " + Remainder);
         }
     }
 }
