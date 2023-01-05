f5212140a3ddc2e5ebdac588c7f12e2144bc9e7e
Rushikesh041 <jadhavrushi041@gmail.com>
2023-01-05 19:31:13 +0530

    [Rushi]VowelorConsonant

--- Basic_Programming/Program.cs
+++ Basic_Programming/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter Alphabet to Check Vowel or Consonant : ");
+            char Alphabet = Convert.ToChar(Console.ReadLine().ToLower());
+            if (Alphabet == 'a' || Alphabet == 'e' || Alphabet == 'i' || Alphabet == 'o' || Alphabet == 'u')
+            {
+                Console.WriteLine(Alphabet + " is Vowel.");
+            }
+            else
+            {
+                Console.WriteLine(Alphabet + " is Consonent.");
+            }
         }
     }
 }
