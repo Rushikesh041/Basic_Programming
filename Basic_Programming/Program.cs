2a0bbf2f242d16dfcc8d0e52fcec7d8becac5972
Rushikesh041 <jadhavrushi041@gmail.com>
2023-01-05 20:09:35 +0530

    [Rushi]Check

--- Basic_Programming/Program.cs
+++ Basic_Programming/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Wel Coome to Basic Programming ");
         }
     }
 }
