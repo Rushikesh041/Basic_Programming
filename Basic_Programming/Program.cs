a8e55839f165448d1c23e026a15b120a7964cf3b
Rushikesh041 <jadhavrushi041@gmail.com>
2023-01-05 18:02:21 +0530

    [Rushi]LeapYear

--- Basic_Programming/Program.cs
+++ Basic_Programming/Program.cs
@@ -1,10 +1,34 @@
 ﻿namespace Basic_Programming
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter the Year to Check Whether Leap Year or Not : ");
+            int Year =Convert.ToInt32(Console.ReadLine());
+            if (Year % 4 == 0)
+            {
+                if (Year % 100 == 0)
+                {
+                    if (Year % 400 == 0)
+                    {
+                        Console.WriteLine("Its a Leap Year");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Its not a Leap Year");
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Its a Leap Year");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Its not a Leap Year");
+            }
         }
     }
 }
