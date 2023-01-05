924a5d340ff0356f109d07407dbf2112bef5e7ba
Rushikesh041 <jadhavrushi041@gmail.com>
2023-01-05 18:39:52 +0530

    [Rushi]EvenOdd

--- Basic_Programming/Program.cs
+++ Basic_Programming/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Enter Number to Check Even or Odd :");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            if (Number % 2 == 0)
+            {
+                Console.WriteLine(Number + " is an Even Number ");
+            }
+            else
+            {
+                Console.WriteLine(Number + " is an Odd Number ");
+            }
         }
     }
 }
