b4ffc93106b70dd8e4f930cb13a1aa3c0066b493
Rushikesh041 <jadhavrushi041@gmail.com>
2023-01-06 17:17:41 +0530

    [Rushi]FlipCoin

--- Basic_Programming/Program.cs
+++ Basic_Programming/Program.cs
@@ -1,10 +1,31 @@
 ﻿namespace Basic_Programming
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Wel Coome to Basic Programming ");
+            int Times = 100;
+            float count1 = 0;
+            float count2 = 0;
+            for (int i = 1; i <= Times; i++)
+            {
+                Random random = new Random();
+                int Flip = random.Next(0, 2);
+
+                if (Flip == 0)
+                {
+                    Console.WriteLine("HEAD");
+                    count1++;
+                }
+                else if (Flip == 1)
+                {
+                    Console.WriteLine("TAILS");
+                    count2++;
+                }
+          
+            }
+            double Percentage = (count1 / count2) * 100;
+            Console.WriteLine("Percentage of Head vs Tail is : " + (Math.Round(Percentage, 2)));
         }
     }
 }
