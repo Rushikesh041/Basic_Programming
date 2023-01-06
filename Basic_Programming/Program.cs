3dcf17490a78a8abfc16805a4ecb234e1429a499
Rushikesh041 <jadhavrushi041@gmail.com>
2023-01-06 18:05:10 +0530

    [Rushi]Factors

--- Basic_Programming/Program.cs
+++ Basic_Programming/Program.cs
@@ -1,10 +1,27 @@
-﻿namespace Basic_Programming
+﻿using System.Numerics;
+
+namespace Basic_Programming
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Wel Coome to Basic Programming ");
+            Console.WriteLine("Enter the Number For Prime Factor : ");
+            int Number = Convert.ToInt32(Console.ReadLine());
+            int Factor;
+            for (Factor = 2; Number>1; Factor++)
+            {
+                if (Number % Factor == 0) 
+                {
+                    int Times = 0;
+                    while (Number % Factor == 0)
+                    {
+                        Number = Number / Factor;
+                        Times++;
+                    }
+                    Console.WriteLine(Factor + " is Prime Factor " + Times + " times ");
+                }
+            }
         }
     }
 }
